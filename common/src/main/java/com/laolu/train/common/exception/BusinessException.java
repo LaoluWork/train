0796ca9fd5aad789202ed6c8ccfc20616260592e
LaoluWork <17876355135@163.com>
2023-07-09 14:49:06 +0800

    4.18 使用自定义异常处理异常业务，不写入堆栈信息，提高性能

--- common/src/main/java/com/laolu/train/common/exception/BusinessException.java
+++ common/src/main/java/com/laolu/train/common/exception/BusinessException.java
@@ -17,4 +17,13 @@ public class BusinessException extends RuntimeException{
         this.e = e;
     }
 
+    /**
+     * 不写入堆栈信息，提高性能
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+
+
 }
