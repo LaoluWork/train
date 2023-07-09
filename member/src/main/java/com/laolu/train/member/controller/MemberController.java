e659359926a066bc8bf71580ddec62acbd2acdf7
LaoluWork <17876355135@163.com>
2023-07-09 17:41:08 +0800

    4.19 集成校验框架validation

--- member/src/main/java/com/laolu/train/member/controller/MemberController.java
+++ member/src/main/java/com/laolu/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.laolu.train.member.controller;
 import com.laolu.train.common.resp.CommonResp;
 import com.laolu.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long id = memberService.register(req);
         return new CommonResp<>(id);
     }
--- member/src/main/java/req/MemberRegisterReq.java
+++ member/src/main/java/req/MemberRegisterReq.java
@@ -1,8 +1,11 @@
 package req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MemberRegisterReq {
+
+    @NotBlank(message = "【手机号】不能为空")
     String mobile;
 }
