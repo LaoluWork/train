d6ebeb847c799262063ef247bcbffe7859b3f436
LaoluWork <17876355135@163.com>
2023-07-18 17:06:21 +0800

    7.5 使用线程本地变量ThreadLocal存储会员信息

--- member/src/main/java/com/laolu/train/member/service/PassengerService.java
+++ member/src/main/java/com/laolu/train/member/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.laolu.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.laolu.train.common.context.LoginMemberContext;
 import com.laolu.train.common.util.SnowUtil;
 import com.laolu.train.member.domain.Passenger;
 import com.laolu.train.member.mapper.PassengerMapper;
@@ -19,6 +20,7 @@ public class PassengerService {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
         passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
