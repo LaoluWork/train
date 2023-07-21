be6220b9082b3e99f415ec18f92a81943cffe588
LaoluWork <17876355135@163.com>
2023-07-21 17:04:17 +0800

    9-10 新增火车座位train_seat表，使用生成器生成增删改查代码提供给admin

--- generator/src/main/java/com/laolu/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/laolu/train/generator/gen/EnumGenerator.java
@@ -2,6 +2,7 @@ package com.laolu.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.laolu.train.business.enums.SeatColEnum;
 import com.laolu.train.business.enums.SeatTypeEnum;
 import com.laolu.train.business.enums.TrainTypeEnum;
 import com.laolu.train.member.enums.PassengerTypeEnum;
@@ -27,6 +28,7 @@ public class EnumGenerator {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
             toJson(TrainTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatTypeEnum.class, bufferObject, bufferArray);
+            toJson(SeatColEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
