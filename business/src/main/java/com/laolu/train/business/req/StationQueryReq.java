8f0f9581e813644c89fe5a68d8aa0880057450f2
LaoluWork <17876355135@163.com>
2023-07-21 11:01:18 +0800

    9-6 新建business业务模块，新增对应数据库和车站表，使用生成器生成增删改查代码提供给admin

---
+++ business/src/main/java/com/laolu/train/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package com.laolu.train.business.req;
+
+import com.laolu.train.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
